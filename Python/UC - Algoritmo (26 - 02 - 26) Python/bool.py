86b7240e0186736651ecfec99a8f136ce473e452
Vitor Emanuel <Vitor00180066100@gmail.com>
2026-03-06 09:10:46 -0300

    Delete Python/UC - Algoritmo (26 - 02 - 26) Python directory

--- Python/UC - Algoritmo (26 - 02 - 26) Python/bool.py
+++
@@ -1,7 +0,0 @@
-resp = input("Você vai passar de ano? S/N: ").strip().lower()
-
-resultado = (resp == "S")
-
-print (type(resultado))
-print("Resultado ", resultado)
-
--- Python/UC - Algoritmo (26 - 02 - 26) Python/exemplo.py
+++
@@ -1,6 +0,0 @@
-base = float (input("Digite a base:"))
-altura = float (input("Digite altura:"))
-
-area = base * altura
-
-print(area)
